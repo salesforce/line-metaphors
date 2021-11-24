90da9e2d4cfdf9d555ee0e572158d7781552bd43
Robert Kosara <rkosara@me.com>
2021-11-23 20:57:41 -0800

    Consent form and tutorial fixes

--- src/Consent.svelte
+++ src/Consent.svelte
@@ -24,10 +24,11 @@
 	<Col md="12">
 		<h1>Informed Consent</h1>
 		<h3>Perception of Effect Sizes in Visualizations</h3>
-		<p>You are invited by Michael Correll of the Tableau Research team to participate in a research study about how users make judgements about information in different kinds of charts. You have been asked to participate because we need to know how trends or effects are perceived in different kinds of charts.</p>
+		<p>You are invited by Robert Kosara and Michael Correll of the Tableau Research team to participate in a research study about how users perceive information in line charts.</p>
 		<p>This study will include participants over 18 years of age who feel comfortable using an online interface to read graphs and answer questions about data.</p>
 		<h4><b>What Will My Participation Involve?</b></h4>
-		<p>If you decide to participate in this research, you will be asked to answer questions about fictional data, using a number of charts or graphs to assist you in making decisions. You will be presented with a set of <span id="chartNum">56</span> charts, and asked to answer brief questions. After the main task, we will ask you some questions about how you read charts and collect basic demographic information. We expect this study to take 30 minutes.</p>
+		<p>If you decide to participate in this research, you will be asked to answer questions about fictional data, using a number of charts or graphs. You will be presented with a total of 36 charts, and asked to give brief descriptions or redraw the charts from memory. After the main task, we will ask you some questions about how you read charts and collect basic demographic information.</p>
+		<p>We expect this study to take about 30 minutes.</p>
 		<h4><b>Are There Any Risks To Me?</b></h4>
 		<p>We don't anticipate any risks from participation in this study greater than normal activity.</p>
 		<h4><b>Are There Any Benefits To Me?</b></h4>
@@ -35,9 +36,9 @@
 		<h4><b>Will I Be Compensated For My Participation?</b></h4>
 		<p>You will receive $4.00 for participating in this study. If you do withdraw prior to the end of the study, you will receive no compensation.</p>
 		<h4><b>Whom Should I Contact If I Have Questions?</b></h4>
-		<p>You may ask any questions about the research at any time. If you have questions about the research after your participation, you should contact the lead researcher, <a href="mailto:mcorrell@tableau.com">Dr. Michael Correll</a> (mcorrell _at_ tableau.com). Your participation is completely voluntary. If you begin participation and change your mind you may end your participation at any time without penalty.</p>
+		<p>You may ask any questions about the research at any time. If you have questions about the research after your participation, you should contact the lead researcher, <a href="mailto:rkosara@tableau.com">Dr. Robert Kosara</a>. Your participation is completely voluntary. If you begin participation and change your mind you may end your participation at any time without penalty.</p>
 		<h4><b>Privacy Statement</b></h4>
-			<p>Your responses to this experiment will be used only by the lead researcher to help us answer questions about chart design. Once collected, your information will be anonymized, and stored on a secure server.</p>
+			<p>Your responses to this experiment will be used only by the two named researchers to help us answer questions about chart design. Once collected, your information will be anonymized and stored on a secure server.</p>
 			<p>While there will probably be publications as a result of this study, your name or participant ID will not be used. Only group characteristics will be published.</p>
 
 		<p>
--- src/Tutorial.svelte
+++ src/Tutorial.svelte
@@ -19,25 +19,53 @@
 	<Col sm="12">
 		<h1>Tutorial for Part {part}</h1>
 		{#if part === 'A'}
-			<p>In the first part of the study, you will be shown charts and then be asked to describe them in a few sentences.
+			<p>In the first part of the study, you will be shown charts about sales of two products in a fictional company, <i>Product A</i> and <i>Product B</i>. You will then be asked to describe each chart in a few sentences.
 				Please try to be specific about patterns and details you observe.</p>
-			<p>You will need to enter a minimum number of characters to be able to advance to the next question.</p>
+			<p>To see the chart, click the <b>Ready</b> button. The input field will appear after a few seconds.</p>
+			<p>You will need to enter a minimum number of characters to be able to advance to the next question. This part of the study consists of nine questions.</p>
 		{:else}
-			<p>In the second part of the study, you will be shown charts and then be asked to recreate them from memory.
-				The charts show the sales prices of two products, A and B, over time.
+			<p>In the second part of the study, you will be shown charts similar to the first part. You will then be asked to recreate each chart from memory.
+				As before, the charts show the sales prices of two products, A and B, over time.
 			</p>
 			<p>You will need to move all of the points in the chart in order to be able to advance to the next question.</p>
 		{/if}
 		<StudyCanvas step={{metaphor: 'approach', style: 'line'}} stepNum={1}
-			clear={part === 'A'} drawAfter={part === 'B'}
-			on:renderDone={part == 'A' ? nextStep : null} on:redrawDone={nextStep} />
+			drawAfter={part === 'B'}
+			on:renderDone={nextStep} on:redrawDone={nextStep} />
 	</Col>
 </Row>
-<Row>
-	<Col sm={{offset:4, size: 4}}>
-		<Button color="primary" disabled={!nextActive} on:click={() => dispatch('done')}>Start Study</Button>
-	</Col>
-</Row>
+{#if nextActive}
+	{#if part === 'B'}
+		<Row>
+			<Col sm={{size: 10, offset: 1}}>
+				<p>Recreate the <span class="blue">blue line</span> in the chart you just saw
+					to the best of your ability by dragging your mouse in the chart above. <br>
+					You will need to move every dot in order to proceed. </p>
+			</Col>
+		</Row>
+	{:else}
+		<Row>
+			<Col sm="12">
+				<p>Describe what you see in the chart above:</p>
+			</Col>
+		</Row>
+		<Row>
+			<Col sm="12">
+				<textarea id="answer" rows="4" cols="60" placeholder="Enter response here…"></textarea> 
+			</Col>
+		</Row>
+		<Row>
+			<Col>
+				<p>Type at least 150 characters to proceed.</p>
+			</Col>
+		</Row>
+	{/if}
+	<Row>
+		<Col sm={{offset:4, size: 4}}>
+			<Button color="primary" disabled={!nextActive} on:click={() => dispatch('done')}>Start Study</Button>
+		</Col>
+	</Row>
+{/if}
 
 <style>
 	p {
