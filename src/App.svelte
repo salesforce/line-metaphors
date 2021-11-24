2605e3ac661864aefc769874c3915a2b17701cc6
Robert Kosara <rkosara@me.com>
2021-11-24 15:35:18 -0800

    Constrain width to make it less ugly

--- src/App.svelte
+++ src/App.svelte
@@ -126,13 +126,7 @@
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 700px;
 		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
